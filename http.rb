4f151b35202ddc7010b9964c531a2b8f59b2e298
Mikhail Yudin <fagci.nsk@gmail.com>
2022-07-02 00:33:43 +0700

    refactor, speedup

--- http.rb
+++ http.rb
@@ -6,7 +6,7 @@ require './lib/stalker'
 TPL = (DATA.each_line.map(&:chomp).join("\r\n") + "\r\n" * 2).freeze
 TITLE_R = /(?<=\<title\>)([^<]+)/i.freeze
 
-Stalker.new(workers: 512).http do |ip, _port, socket|
+Stalker.new(workers: 256, connect_timeout: 0.33).http do |ip, _port, socket|
   socket.print(TPL % ip)
   title = socket.read.match(TITLE_R).to_s.strip
   puts "#{ip} #{title}" unless title.empty?
