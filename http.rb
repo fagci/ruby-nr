8ebdd414dd5006ce647a390f550ed0c55d97e29c
Mikhail Yudin <fagci.nsk@gmail.com>
2022-07-01 23:39:24 +0700

    refactor

--- http.rb
+++ http.rb
@@ -3,12 +3,13 @@
 require 'socket'
 require './lib/stalker'
 
-TPL = DATA.each_line.map(&:chomp).join("\r\n")
+TPL = DATA.each_line.map(&:chomp).join("\r\n")+ "\r\n" * 2
+TITLE_R = /(?<=\<title\>)([^<]+)/i
 
-Stalker.new(workers: 512).http do |ip, port, socket|
-  socket.print(TPL % ip + "\r\n" * 2)
-  title = socket.read.match(/(?<=\<title\>)([^<]+)/i).to_s.strip
-puts "#{ip}:#{port} #{title}" unless title.empty?
+Stalker.new(workers: 512).http do |ip, _port, socket|
+  socket.print(TPL % ip)
+  title = socket.read.match(TITLE_R).to_s.strip
+  puts "#{ip} #{title}" unless title.empty?
 end
 
 __END__
