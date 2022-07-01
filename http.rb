26466d8a0da3fa24451967e10e21b27cf4997ee2
Mikhail Yudin <fagci.nsk@gmail.com>
2022-07-01 23:20:34 +0700

    refactor

--- http.rb
+++ http.rb
@@ -7,8 +7,8 @@ TPL = DATA.each_line.map(&:chomp).join("\r\n")
 
 Stalker.new(workers: 512).http do |ip, port, socket|
   socket.print(TPL % ip + "\r\n" * 2)
-  title = socket.read.match(/(?<=\<title\>)([^<]+)/i)
-  puts "#{ip}:#{port} #{title}" if title
+  title = socket.read.match(/(?<=\<title\>)([^<]+)/i).to_s.strip
+puts "#{ip}:#{port} #{title}" unless title.empty?
 end
 
 __END__
