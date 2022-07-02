c7ce06248c834b21a979017297ac0f8ec8336b20
Mikhail Yudin <fagci.nsk@gmail.com>
2022-07-02 17:47:58 +0700

    feat: multiproc threading (beta)

--- lib/stalker.rb
+++ lib/stalker.rb
@@ -1,3 +1,4 @@
+require 'etc'
 require './lib/gen'
 
 # Netstalking tool
@@ -5,6 +6,9 @@ class Stalker
   def initialize(options = {})
     @connect_timeout = options.fetch(:connect_timeout, 0.75)
     @workers_count = options.fetch(:workers, 64)
+    @proc_count = Etc.nprocessors
+    @thr_per_proc = @workers_count / @proc_count
+    puts "Thr: #{@workers_count}, proc: #{@proc_count}, thr/proc: #{@thr_per_proc}"
   end
 
   # TODO: doc + get services from source
@@ -15,15 +19,20 @@ class Stalker
   }
   services.each do |svc, port|
     define_method(svc) do |&block|
-      work(port, &block)
+      @proc_count.times do
+        Process.fork do
+          workers = (1..@thr_per_proc).map do
+            ::Thread.new { worker port, &block }
+          end
+          workers.map(&:join)
+        rescue Interrupt => e
+          # puts "Exiting..."
+        end
+      end
+      Process.waitall
+    rescue Interrupt => e
+      # puts "Exiting..."
     end
-  end
-
-  def work(port, &block)
-    workers = (1..@workers_count).map do
-      ::Thread.new { worker port, &block }
-    end
-    workers.map(&:join)
   end
 
   def worker(port)
@@ -32,7 +41,7 @@ class Stalker
       s = ::Socket.tcp(ip, port, connect_timeout: @connect_timeout)
       yield(ip, port, s)
       s.close
-    rescue Errno::ECONNREFUSED, Errno::ETIMEDOUT, Errno::EHOSTUNREACH, Errno::ENETUNREACH, Errno::ECONNRESET
+      rescue Errno::ECONNREFUSED, Errno::ETIMEDOUT, Errno::EHOSTUNREACH, Errno::ENETUNREACH, Errno::ECONNRESET
       next
     end
   end
